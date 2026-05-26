216b2c1c49b1c144e02f861225a48b91d82fca78
kgideon084-maker <kgideon084@gmail.com>
2026-05-26 14:55:58 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,71 @@
+body{
+  font-family: Arial;
+  margin:0;
+  background:#f4f4f4;
+}
+
+header{
+  background:#0d6efd;
+  color:white;
+  text-align:center;
+  padding:25px;
+}
+
+nav{
+  background:#222;
+  display:flex;
+  justify-content:center;
+}
+
+nav a{
+  color:white;
+  text-decoration:none;
+  padding:15px;
+}
+
+nav a:hover{
+  background:#0d6efd;
+}
+
+section{
+  background:white;
+  margin:20px;
+  padding:20px;
+  border-radius:10px;
+}
+
+h2{
+  color:#0d6efd;
+}
+
+form{
+  display:flex;
+  flex-direction:column;
+}
+
+input, select, textarea{
+  padding:12px;
+  margin:10px 0;
+  border:1px solid #ccc;
+  border-radius:5px;
+}
+
+button{
+  background:#0d6efd;
+  color:white;
+  border:none;
+  padding:12px;
+  cursor:pointer;
+  border-radius:5px;
+}
+
+button:hover{
+  background:#084298;
+}
+
+footer{
+  background:#222;
+  color:white;
+  text-align:center;
+  padding:15px;
+    }
